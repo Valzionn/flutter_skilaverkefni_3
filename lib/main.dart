752879ab0e6f9048b90fd1b7185e523bd94026b3
Valzionn <144949469+Valzionn@users.noreply.github.com>
2023-11-22 20:11:48 +0000

    blurv

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_skilaverkefni_3/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor:const Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      darkTheme: ThemeData.dark().copyWith(useMaterial3: true,
+      colorScheme: kDarkColorScheme,
+      ),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 16,
+          ),
+        )),
+      //default themeMode: ThemeMode.system, //default
       home: const Expenses(),
     ),
   );
 }
+//playerName
+// youDied
+// youSurvived
+// torch
+// ---- navigation/pages -----
+// startGame
+// ogreCaveEnterance
+// ogreCaveFirstBranch
+// wayOne
+// wayTwo
+// wayThree
+// ogreLair
+// ogreTrove
+// ogreTrap
+// ----interactables-----
+// loot
+// 	magic sword
+// 	gold
+// frontalAttack
+// sneakAttack
+// 	successful
+// 	failed
+// ---- reloads -----
+// tryAgain
+// victoryScreen 
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -6,9 +6,11 @@ class ExpensesList extends StatelessWidget {
   const ExpensesList({
     super.key,
     required this.expenses,
+    required this.onRemoveExpense,
   });
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +18,15 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
-        onDismissed: ,
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.20),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index],);
+        },
         child: ExpenseItem(
           expenses[index],
         ),
