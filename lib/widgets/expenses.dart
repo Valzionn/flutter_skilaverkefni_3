f44824556b2f5c3f979998180eda911bb038dda7
Valzionn <144949469+Valzionn@users.noreply.github.com>
2023-11-22 15:22:12 +0000

    hmmm

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,9 +30,20 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
-       builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    
   }
 
   @override
--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_skilaverkefni_3/models/expense.dart';
 
